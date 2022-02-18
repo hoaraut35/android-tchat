6cab262f1ba4301350783c2f012415dacecdc3d8
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2022-02-18 13:30:56 +0100

    initial commit

--- build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
+++ build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
@@ -15,25 +15,38 @@
  */
 package com.google.firebase.codelab.friendlychat
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig.*
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.codelab.friendlychat.BuildConfig
 import com.google.firebase.codelab.friendlychat.databinding.ActivityMainBinding
+import com.google.firebase.codelab.friendlychat.model.FriendlyMessage
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var manager: LinearLayoutManager
-
+    private lateinit var auth: FirebaseAuth
     private val openDocument = registerForActivityResult(MyOpenDocumentContract()) { uri ->
         onImageSelected(uri)
     }
+
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: FriendlyMessageAdapter
 
     // TODO: implement Firebase instance variables
 
@@ -47,9 +60,38 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Firebase Auth and check if the user is signed in
         // TODO: implement
+        auth = Firebase.auth
+        if (auth.currentUser == null){
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
 
         // Initialize Realtime Database and FirebaseRecyclerAdapter
         // TODO: implement
+        // Initialize Realtime Database
+        db = Firebase.database
+        val messagesRef = db.reference.child(MESSAGES_CHILD)
+
+// The FirebaseRecyclerAdapter class and options come from the FirebaseUI library
+// See: https://github.com/firebase/FirebaseUI-Android
+        val options = FirebaseRecyclerOptions.Builder<FriendlyMessage>()
+            .setQuery(messagesRef, FriendlyMessage::class.java)
+            .build()
+        adapter = FriendlyMessageAdapter(options, getUserName())
+        binding.progressBar.visibility = ProgressBar.INVISIBLE
+        manager = LinearLayoutManager(this)
+        manager.stackFromEnd = true
+        binding.messageRecyclerView.layoutManager = manager
+        binding.messageRecyclerView.adapter = adapter
+
+// Scroll down when a new message arrives
+// See MyScrollToBottomObserver for details
+        adapter.registerAdapterDataObserver(
+            MyScrollToBottomObserver(binding.messageRecyclerView, adapter, manager)
+        )
+
+
 
         // Disable the send button when there's no text in the input field
         // See MyButtonObserver for details
@@ -68,14 +110,21 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         // Check if user is signed in.
         // TODO: implement
+        if(auth.currentUser == null){
+            startActivity(Intent(this,SignInActivity::class.java))
+            finish()
+            return
+        }
     }
 
     public override fun onPause() {
+        adapter.stopListening()
         super.onPause()
     }
 
     public override fun onResume() {
         super.onResume()
+        adapter.startListening()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -94,6 +143,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun getPhotoUrl(): String? {
+        val user = auth.currentUser
+        return user?.photoUrl?.toString()
+    }
+
+    private fun getUserName(): String? {
+        val user = auth.currentUser
+        return if (user != null) {
+            user.displayName
+        } else ANONYMOUS
+    }
+
     private fun onImageSelected(uri: Uri) {
         // TODO: implement
     }
@@ -105,6 +166,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun signOut() {
         // TODO: implement
+        AuthUI.getInstance().signOut(this)
+        startActivity(Intent(this, SignInActivity::class.java))
+        finish()
     }
 
     companion object {
